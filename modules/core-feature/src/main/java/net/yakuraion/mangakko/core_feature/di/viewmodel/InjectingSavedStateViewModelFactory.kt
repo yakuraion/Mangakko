cb8fbe397e05ac6b9aa05f52915dfa96b5f679f1
Slava Yakovlev <yakuraion@gmail.com>
2021-09-02 00:44:23 +0300

    fix InjectingSavedStateViewModelFactory with holding model

--- modules/core-feature/src/main/java/net/yakuraion/mangakko/core_feature/di/viewmodel/InjectingSavedStateViewModelFactory.kt
+++ modules/core-feature/src/main/java/net/yakuraion/mangakko/core_feature/di/viewmodel/InjectingSavedStateViewModelFactory.kt
@@ -1,10 +1,11 @@
 package net.yakuraion.mangakko.core_feature.di.viewmodel
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.savedstate.SavedStateRegistryOwner
+import androidx.lifecycle.ViewModelProvider
 import dagger.Reusable
 import javax.inject.Inject
 import javax.inject.Provider
@@ -19,10 +20,10 @@ class InjectingSavedStateViewModelFactory @Inject constructor(
     @Suppress("UNCHECKED_CAST")
     fun <VM : ViewModel> create(
         viewModelClass: KClass<out ViewModel>,
-        owner: SavedStateRegistryOwner,
+        fragment: Fragment,
         defaultArgs: Bundle? = null
     ): VM {
-        val factory = object : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+        val factory = object : AbstractSavedStateViewModelFactory(fragment, defaultArgs) {
 
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel?> create(
@@ -40,7 +41,7 @@ class InjectingSavedStateViewModelFactory @Inject constructor(
             }
         }
         try {
-            return factory.create(viewModelClass.java) as VM
+            return ViewModelProvider(fragment, factory)[viewModelClass.java] as VM
         } catch (e: Exception) {
             throw RuntimeException(e)
         }

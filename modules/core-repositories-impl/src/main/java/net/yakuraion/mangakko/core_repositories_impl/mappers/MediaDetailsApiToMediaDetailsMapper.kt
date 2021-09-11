6fe4cbde8bc9be2f71bb50d951df4da83d6fe11f
Slava Yakovlev <yakuraion@gmail.com>
2021-09-11 14:19:13 +0300

    remove html tags from media description

--- modules/core-repositories-impl/src/main/java/net/yakuraion/mangakko/core_repositories_impl/mappers/MediaDetailsApiToMediaDetailsMapper.kt
+++ modules/core-repositories-impl/src/main/java/net/yakuraion/mangakko/core_repositories_impl/mappers/MediaDetailsApiToMediaDetailsMapper.kt
@@ -1,6 +1,9 @@
 package net.yakuraion.mangakko.core_repositories_impl.mappers
 
 import android.graphics.Color
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.text.Html
 import net.yakuraion.mangakko.core_entity.MediaDetails
 import net.yakuraion.mangakko.core_network.QueryMediaDetailsQuery
 import net.yakuraion.mangakko.core_network.type.MediaRankType
@@ -16,7 +19,7 @@ object MediaDetailsApiToMediaDetailsMapper {
             imageUrl = value.coverImage()?.extraLarge().orEmpty(),
             mainColor = value.coverImage()?.color()?.let { Color.parseColor(it) },
             score = value.meanScore(),
-            description = value.description().orEmpty().replace("<br>", ""),
+            description = value.description().orEmpty().removeHtmlTags(),
             rateRank = value.getRankOnAllTime(RATED),
             popularityRank = value.getRankOnAllTime(POPULAR)
         )
@@ -24,5 +27,14 @@ object MediaDetailsApiToMediaDetailsMapper {
 
     private fun QueryMediaDetailsQuery.Media.getRankOnAllTime(type: MediaRankType): Int? {
         return rankings()?.find { it.allTime() == true && it.type() == type }?.rank()
+    }
+
+    private fun String.removeHtmlTags(): String {
+        return if (VERSION.SDK_INT >= VERSION_CODES.N) {
+            Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+        } else {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(this).toString()
+        }
     }
 }

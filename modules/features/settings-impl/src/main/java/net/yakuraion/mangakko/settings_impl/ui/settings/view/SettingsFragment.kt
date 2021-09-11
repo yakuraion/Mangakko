db977b6773a81dc72142b6c43ee1f76de0af238b
Slava Yakovlev <yakuraion@gmail.com>
2021-09-12 00:36:35 +0300

    add theme radio group to SettingsFragment

--- modules/features/settings-impl/src/main/java/net/yakuraion/mangakko/settings_impl/ui/settings/view/SettingsFragment.kt
+++ modules/features/settings-impl/src/main/java/net/yakuraion/mangakko/settings_impl/ui/settings/view/SettingsFragment.kt
@@ -3,6 +3,10 @@ package net.yakuraion.mangakko.settings_impl.ui.settings.view
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import kotlinx.android.synthetic.main.settings_fragment_settings.rootLayout
 import net.yakuraion.mangakko.core_feature.di.viewmodel.InjectingSavedStateViewModelFactory
 import net.yakuraion.mangakko.core_feature.ui.base.BaseFragment
 import net.yakuraion.mangakko.settings_impl.R
@@ -26,6 +30,15 @@ class SettingsFragment : BaseFragment<SettingsViewModel>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpInsets()
+    }
+
+    private fun setUpInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = insets.top)
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     companion object {
